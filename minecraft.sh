1c2f3b4eec08cac5f907f2994ebeb31da025d1e1
Charles-IX <78650751+Charles-IX@users.noreply.github.com>
2024-01-24 17:34:18 +0800

    v0.16 is here.

--- minecraft.sh
+++ minecraft.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-lock_file=/home/charles/Minecraft/lock
-ver=0.15
-remote_url="Input your own MC url here"
+ver=0.16
+config_file=/home/$USER/.minecraft/raspi_mc_launcher_conf.sh
+
 trap 'echo "" && echo "" && exit 0' SIGINT
 
 echo ""
@@ -10,21 +10,95 @@ echo "Minecraft server launcher v$ver by Charles"
 date
 echo "==========================================="
 
+if (( EUID == 0)); then
+    echo "The script must not be run as root."
+    exit 1
+fi
+
+if [ -e "$config_file" ]; then
+    source $config_file
+else
+    echo "Hello there,"
+    echo "it seems that you are running the launcher for the first time."
+    echo ""
+    echo "In order to function normally, the launcher will attempt to write a config file"
+    echo "located in ~/.minecraft/raspi_mc_launcher_conf.sh"
+    echo "Enter your path to the Minecraft directory, or type 'yes' (with no quote) to use the default"
+    echo "~/Minecraft"
+    while true; do
+        IFS= read -r path
+        if [ -e "$path" ]; then
+            if [ -d "$path" ]; then
+            echo "Path successfully set to $path ."
+            fi
+            break;
+        elif [ "$path" == "yes" ]; then
+            path=/home/$USER/Minecraft
+        fi
+        echo "You should enter a path to the Minecraft directory, not a file, and do not add quotation marks."
+        echo "You can also just type 'yes' to use /home/$USER/Minecraft ."
+    done
+    echo "Then you can paste your remote url here so the launcher can remind you when the server has launched"
+    echo "like this: 'Connect <your url> remotely to join the game.'"
+    echo "...Or simply press Enter to skip this."
+    read -r url
+    if [ $url != "" ]; then
+        echo "Url successfully set to $url ."
+    else
+        echo "Url input skipped."
+    fi
+    echo -e "path=$path\nlock_file=$path/lock\nremote_url=$url\n" > $config_file
+    echo "You can always start over by deleting $config_file ."
+    echo "Now the launcher will attempt to copy itself to /usr/local/bin/mc "
+    echo "so next time you can just type 'mc' to run the launcher."
+    sudo cp -v $0 /usr/local/bin/mc
+    echo ""
+    source $config_file
+fi
+
+ver_2=$(grep "ver=" $path/minecraft.sh | awk -F "ver=" '{print $2}')
+if [ "$ver_2" != "" ]; then
+    if (( ver_2 > ver )); then
+        echo "A newer version of Minecraft server launcher detected in $path/minecraft.sh ."
+        echo "Current version: $ver"
+        echo "New version: $ver_2"
+        echo "Upgrade /usr/local/bin/mc now?"
+        while true; do
+            echo "Yes, No?"
+            read -p "[ Y / N ]: " upgrade
+            case $upgrade in
+                Y|y)
+                    echo "Will attempt to upgrade /usr/local/bin/mc ."
+                    sudo cp -v $path/minecraft.sh /usr/local/bin/mc
+                    echo "Run the launcher again to use the newer version."
+                    exit 0
+                    ;;
+                N|n)
+                    echo "Upgrade skipped."
+                    break
+                    ;;
+            esac
+        done
+    echo ""
+    fi
+fi
 
 if [ -e "$lock_file" ]; then
     echo "A Minecraft server instance seems already running, located in"
     cat "$lock_file"
-    echo "Connect $remote_url remotely to join the game."
+    if [ "$url" != "" ]; then
+        echo "Connect $remote_url remotely to join the game."
+    fi
     service="$(cat $(cat $lock_file)/service)"
     status=$(systemctl show -p ActiveState --value $service)
     screen_log="$(cat $lock_file)/screen_log"
 
     if [ "$status" != "active" ]; then
-	echo "However, $service has run into '$status' state."
-	echo "You can check systemctl for details, or simply remove the lock file to perform manual restart."
-	echo "You can also try to access to the Minecraft server console, but chances are the server is down."
-    echo "Latest screen session is recorded in '$screen_log'." 
-	while true; do
+	    echo "However, $service has run into '$status' state."
+	    echo "You can check systemctl for details, or simply remove the lock file to enable a manual restart."
+	    echo "You can also try to access to the Minecraft server console, but chances are the server is down."
+        echo "Latest screen session is recorded in '$screen_log'." 
+	    while true; do
             echo "Service status, Remove lock, show Console, show Log, Quit?"
             read -p "[ S / R / C / L / Q ]: " op
 
@@ -73,7 +147,7 @@ if [ -e "$lock_file" ]; then
 	        C|c)
 	            echo "Enter Ctrl+A then 'D' to return to terminal."
 	            sleep 3
-	            sudo -u $USER screen -r minecraft
+	            sudo -u charles screen -r minecraft
 	            #break
 	            ;;
 	        Q|q)
@@ -85,7 +159,7 @@ if [ -e "$lock_file" ]; then
         done
     fi
 
-else
+else    # In this section you can manually add your servers.
     echo "Please select one server to run."
     echo "1. Minecraft 1.20.1 Forge"
     echo "2. Minecraft 1.20.4 Vanilla"
@@ -98,21 +172,22 @@ else
             1)
                 echo "Running Minecraft 1.20.1 Forge"
        	        systemctl start minecraft_1.20.1
-	        systemctl status minecraft_1.20.1 --no-pager
-	        break
-	        ;;
+	            systemctl status minecraft_1.20.1 --no-pager
+	            break
+	            ;;
             2)
-	        echo "Running Minecraft 1.20.4 Vanilla"
-	        systemctl start minecraft_1.20.4
-	        systemctl status minecraft_1.20.4 --no-pager
-	        break
-	        ;;
-	    Q|q)
-	        break
-	        ;;
+	            echo "Running Minecraft 1.20.4 Vanilla"
+	            systemctl start minecraft_1.20.4
+	            systemctl status minecraft_1.20.4 --no-pager
+	            break
+	            ;;
+	        Q|q)
+	            break
+	            ;;
             *)
-	        echo "Invalid option. Try again."
+	            echo "Invalid option. Try again."
         esac
     done
 fi
+
 echo ""
