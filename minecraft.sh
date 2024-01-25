1946def3ede705b402530ed0602feef7d7cc2433
Charles-IX <78650751+Charles-IX@users.noreply.github.com>
2024-01-25 15:38:38 +0800

    v0.18.0 is ready.

--- minecraft.sh
+++ minecraft.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ver=0.16.7
+ver=0.18.0
 config_file=/home/$USER/.minecraft/raspi_mc_launcher_conf.sh
 
 trap 'echo "" && echo "" && exit 0' SIGINT
@@ -10,13 +10,70 @@ echo "Minecraft server launcher v$ver by Charles"
 date
 echo "==========================================="
 
-if (( EUID == 0)); then
+
+# Detect whether is running by root, if so, exit with code 1.
+if (( EUID == 0 )); then
     echo "The script must not be run as root."
     echo ""
     echo ""
     exit 1
 fi
 
+
+# Handle parameters.
+if (( $# > 1 )); then
+    echo "0 or 1 parameters expected, got $#."
+else
+    case $1 in
+        "-h"|"--help")
+            echo "This is a simple Bash script aiming to simplify the control the start/stop of several Minecraft Java Edition servers."
+            if [ -e "$config_file" ]; then
+                source $config_file
+                echo "Config file: /home/$USER/.minecraft/raspi_mc_launcher_conf.sh"
+                echo "Current Minecraft folder: $path"
+            else
+                echo "Config file does not exist. Is this the first time you run the script?"
+                echo "If so, run with no parameter to create a new config."
+            fi
+            echo "For more information, please visit https://github.com/Charles-IX/RaspberryPi_MC_launcher ."
+            echo ""
+            echo "Available parameters:"
+            echo "-h --help :  Print this help information."
+            echo "-u --update :  Try to update minecraft.sh in your Minecraft folder using wget."
+            echo ""
+            echo ""
+            exit 0
+            ;;
+        "-u"|"--update")
+            echo "Trying to fetch latest minecraft.sh from GitHub, and will replace the current one in your Minecraft folder."
+            echo ""
+            if [ -e "$config_file" ]; then
+                source $config_file
+                wget -O $path/minecraft.sh https://raw.githubusercontent.com/Charles-IX/RaspberryPi_MC_launcher/main/minecraft.sh
+                echo ""
+                echo "Run the launcher again by typing 'mc' in the terminal."
+                echo "If you are not running the latest version, you will be prompted to upgrade /usr/local/bin/mc ."
+                echo ""
+                echo ""
+                exit 0
+            else
+                echo "Config file does not exist, don't know where to put minecraft.sh ."
+                echo ""
+                # echo "Is this the first time you run the script? If so, run with no parameter to create a new config."
+                break
+            fi
+            ;;
+        *)
+            echo "Unexpected parameter. Run 'mc -h' or 'mc --help' for help."
+            echo ""
+            echo ""
+            exit 2
+    esac
+fi
+
+
+
+# Try to read config file, will prompt to create one if does not exist.
 if [ -e "$config_file" ]; then
     source $config_file
 else
@@ -59,16 +116,19 @@ else
     else
         echo "Url input skipped."
     fi
-    
+
     echo -e "path=$path\nlock_file=$path/lock\nremote_url=$url\n" > $config_file
     echo ""
     echo "You can always start over by deleting $config_file ."
     echo "Now the launcher will attempt to copy itself to /usr/local/bin/mc so next time you can just type 'mc' to run the launcher."
+    echo ""
     sudo cp -v $0 /usr/local/bin/mc
     echo ""
     source $config_file
 fi
 
+
+# Notify the user if there is newer version in Minecraft folder.
 ver_2=$(grep "ver=" $path/minecraft.sh | awk -F "ver=" '{print $2}' | head -n1)
 if [ "$ver_2" != "" ]; then
     if [ "$(printf '%s\n' "$ver_2" "$ver" | sort -V | head -n1)" = "$ver" ]; then
@@ -83,7 +143,9 @@ if [ "$ver_2" != "" ]; then
                 case $upgrade in
                     Y|y)
                         echo "Will attempt to upgrade /usr/local/bin/mc ."
+                        echo ""
                         sudo cp -v $path/minecraft.sh /usr/local/bin/mc
+                        echo ""
                         echo "Run the launcher again to use the newer version."
                         echo ""
                         echo ""
@@ -100,6 +162,8 @@ if [ "$ver_2" != "" ]; then
     fi
 fi
 
+
+# If there is a running server instance, do not run a new one.
 if [ -e "$lock_file" ]; then
     echo "A Minecraft server instance seems already running, located in"
     cat "$lock_file"
@@ -132,7 +196,7 @@ if [ -e "$lock_file" ]; then
                 C|c)
                     echo "Enter Ctrl+A then 'D' to return to terminal."
                     sleep 3
-                    sudo -u charles screen -r minecraft
+                    sudo -u $USER screen -r minecraft
                     #break
                     ;;
                 L|l)
@@ -164,7 +228,7 @@ if [ -e "$lock_file" ]; then
 	        C|c)
 	            echo "Enter Ctrl+A then 'D' to return to terminal."
 	            sleep 3
-	            sudo -u charles screen -r minecraft
+	            sudo -u $USER screen -r minecraft
 	            #break
 	            ;;
 	        Q|q)
