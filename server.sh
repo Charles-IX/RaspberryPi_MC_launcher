42b15a8dd4b38472dd5a4149fbbe9ae769620792
Charles-IX <78650751+Charles-IX@users.noreply.github.com>
2024-01-26 10:58:32 +0800

    This allows users to define their servers.

---
+++ server.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "1. Minecraft 1.20.1 Forge"
+echo "2. Minecraft 1.20.4 Vanilla"
+
+name_1="Minecraft 1.20.1 Forge"
+service_1="minecraft_1.20.1"
+
+name_2="Minecraft 1.20.4 Vanilla"
+service_2="minecraft_1.20.4"
